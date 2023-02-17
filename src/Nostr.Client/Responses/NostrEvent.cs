805901898cdb802674a4d001e02252059f468d03
Mariusz Kotas <m@mkotas.cz>
2023-02-18 00:34:12 +0100

    Extend `QueryTool` page

--- src/Nostr.Client/Responses/NostrEvent.cs
+++ src/Nostr.Client/Responses/NostrEvent.cs
@@ -5,7 +5,7 @@ using Nostr.Client.Messages;
 namespace Nostr.Client.Responses
 {
     [DebuggerDisplay("{CreatedAt} {Kind.ToString()} {Pubkey}")]
-    public class NostrEvent
+    public class NostrEvent : IEquatable<NostrEvent>
     {
         /// <summary>
         /// 32-bytes lowercase hex-encoded sha256 of the the serialized event data
@@ -39,5 +39,25 @@ namespace Nostr.Client.Responses
         /// </summary>
         [JsonExtensionData]
         public Dictionary<string, object> AdditionalData { get; init; } = new();
+
+        public bool Equals(NostrEvent? other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((NostrEvent)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Id != null ? Id.GetHashCode() : 0);
+        }
     }
 }
