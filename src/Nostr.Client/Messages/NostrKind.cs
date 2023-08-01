22ff97c44ee23f247d56e406405e33755acb7c8f
Mariusz Kotas <m@mkotas.cz>
2023-08-01 16:47:56 +0200

    Add more Nostr kinds

--- src/Nostr.Client/Messages/NostrKind.cs
+++ src/Nostr.Client/Messages/NostrKind.cs
@@ -12,6 +12,8 @@
         Reaction = 7,
         BadgeAward = 8,
 
+        GenericRepost = 16,
+
         // nip-28 public chat
         ChannelCreation = 40,
         ChannelMetadata = 41,
@@ -21,23 +23,38 @@
 
         // nip-28 public chat reserved [45-49]
 
+        FileMetadata = 1063,
         LiveChatMessage = 1311,
 
         Reporting = 1984,
+        Label = 1985,
 
         ZapRequest = 9734,
         Zap = 9735,
 
+        MuteList = 10000,
+        PinList = 10001,
         RelayListMetadata = 10002,
+
+        WalletInfo = 13194,
         ClientAuthentication = 22242,
+        WalletRequest = 23194,
+        WalletResponse = 23195,
         NostrConnect = 24133,
+        HttpAuth = 27235,
+
+        CategorizedPeopleList = 30000,
+        CategorizedBookmarkList = 30001,
 
         ProfileBadges = 30008,
         BadgeDefinition = 30009,
 
         LongFormContent = 30023,
+        DraftLongFormContent = 30024,
 
-        ApplicationSpecificData = 30078
+        ApplicationSpecificData = 30078,
+
+        LiveEvent = 30311,
 
         // nip-16 regular events                   [ 1000- 9999]
         // nip-16 replaceable events               [10000-19999]
